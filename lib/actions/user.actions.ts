619a7c4fca000a5db304b46aa24dd10a77b0da4e
Sze Yan  How <szeyanhow@Szes-MacBook-Pro.local>
2023-10-12 20:56:44 -0400

    update

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,7 +3,7 @@
 import { FilterQuery, SortOrder } from "mongoose";
 import { revalidatePath } from "next/cache";
 
-import Community from "../models/community.model.ts";
+import Community from "../models/community.model";
 import Thread from "../models/thread.model";
 import User from "../models/user.model";
 
